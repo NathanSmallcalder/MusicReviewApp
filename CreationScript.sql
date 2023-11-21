d5d5e185b78f41dce9c68bc10e83deb0d5f4bf76
Nathan Smallcalder <74361286+NathanSmallcalder@users.noreply.github.com>
2023-11-21 22:49:11 +0000

    Add files via upload

--- CreationScript.sql
+++ CreationScript.sql
@@ -1,60 +1,66 @@
+CREATE DATABASE IF NOT EXISTS musicDatabase;
 USE musicDatabase;
-
 
 -- Create User table
 CREATE TABLE User (
-    UserID INT PRIMARY KEY,
-    Username VARCHAR(255),
-    Email VARCHAR(255),
-    Password VARCHAR(255)
+    UserID INT PRIMARY KEY AUTO_INCREMENT,
+    Username VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL,
+    PasswordHash VARCHAR(40) NOT NULL
+);
+
+-- Create Artist table
+CREATE TABLE Artist (
+    ArtistID VARCHAR(255) PRIMARY KEY,
+    ArtistName VARCHAR(255) NOT NULL,
+    Genre VARCHAR(255) NOT NULL
 );
 
 -- Create Album table
 CREATE TABLE Album (
-    AlbumID INT PRIMARY KEY,
-    Title VARCHAR(255),
-    Artist VARCHAR(255),
-    ReleaseDate DATE,
-    Genre VARCHAR(255),
-    CoverImage VARCHAR(255)
+    AlbumID INT PRIMARY KEY AUTO_INCREMENT,
+    Title VARCHAR(255) NOT NULL,
+    ReleaseDate VARCHAR(25),
+    CoverImage VARCHAR(255),
+    ArtistID VARCHAR(255),
+    FOREIGN KEY (ArtistID) REFERENCES Artist(ArtistID)
 );
 
 -- Create Review table
 CREATE TABLE Review (
-    ReviewID INT PRIMARY KEY,
+    ReviewID INT PRIMARY KEY AUTO_INCREMENT,
     UserID INT,
     AlbumID INT,
     Rating INT,
     Comment TEXT,
-    DatePosted DATE,
+    DatePosted DATETIME,
     FOREIGN KEY (UserID) REFERENCES User(UserID),
     FOREIGN KEY (AlbumID) REFERENCES Album(AlbumID)
 );
 
 -- Create Song table
 CREATE TABLE Song (
-    SongID INT PRIMARY KEY,
-    Title VARCHAR(255),
-    Artist VARCHAR(255),
+    SongID INT PRIMARY KEY AUTO_INCREMENT,
+    Title VARCHAR(255) NOT NULL,
     AlbumID INT,
-    Duration INT,
-    ReleaseDate DATE,
-    FOREIGN KEY (AlbumID) REFERENCES Album(AlbumID)
+    ArtistID VARCHAR(255),
+    FOREIGN KEY (AlbumID) REFERENCES Album(AlbumID),
+    FOREIGN KEY (ArtistID) REFERENCES Artist(ArtistID)
 );
 
 -- Create UserAlbumList table
 CREATE TABLE UserAlbumList (
-    ListID INT PRIMARY KEY,
+    ListID INT PRIMARY KEY AUTO_INCREMENT,
     UserID INT,
     ListName VARCHAR(255),
     Description TEXT,
-    CreatedDate DATE,
+    CreatedDate DATETIME,
     FOREIGN KEY (UserID) REFERENCES User(UserID)
 );
 
 -- Create UserAlbumListDetail table
 CREATE TABLE UserAlbumListDetail (
-    DetailID INT PRIMARY KEY,
+    DetailID INT PRIMARY KEY AUTO_INCREMENT,
     ListID INT,
     AlbumID INT,
     OrderIndex INT,
@@ -62,24 +68,24 @@ CREATE TABLE UserAlbumListDetail (
     FOREIGN KEY (AlbumID) REFERENCES Album(AlbumID)
 );
 
--- Create SongReview table
+-- Create SongReview table for individual song reviews
 CREATE TABLE SongReview (
-    SongReviewID INT PRIMARY KEY,
+    SongReviewID INT PRIMARY KEY AUTO_INCREMENT,
     UserID INT,
     SongID INT,
     Rating INT,
     Comment TEXT,
-    DatePosted DATE,
+    DatePosted DATETIME,
     FOREIGN KEY (UserID) REFERENCES User(UserID),
     FOREIGN KEY (SongID) REFERENCES Song(SongID)
 );
 
--- Create Follower table
-CREATE TABLE Follower (
-    FollowerID INT PRIMARY KEY,
+-- Create Following table
+CREATE TABLE Following (
+    FollowingID INT PRIMARY KEY AUTO_INCREMENT,
     FollowerUserID INT,
     FollowedUserID INT,
-    DateFollowed DATE,
+    DateFollowed DATETIME,
     FOREIGN KEY (FollowerUserID) REFERENCES User(UserID),
     FOREIGN KEY (FollowedUserID) REFERENCES User(UserID)
 );
