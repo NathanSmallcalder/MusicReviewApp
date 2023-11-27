3e436bc68d84546c2a5175d8d36e9eea9314deaa
Nathan Smallcalder <74361286+NathanSmallcalder@users.noreply.github.com>
2023-11-27 11:56:03 +0000

    Add files via upload

--- dataCollection.py
+++ dataCollection.py
@@ -34,45 +34,55 @@ related_artists = related_artist_ids(related)
 
 
 for artists in related_artists:
-    """ 
-    Iterates through artists and inserts them into the database
-    """
-    artist_albums = get_artist_albums(token,artists['ArtistId'])
-
-    albums_list = forge_album_tracklist(artist_albums, token)
-    sql_insert = "INSERT INTO Artist (ArtistID, ArtistName, Genre) VALUES (%s, %s, %s)"
-    s = ''.join(str(x) for x in artists['genres'])
-    cursor.execute(sql_insert, (artists['ArtistsId'], artists['ArtistName'], s))
-    for albums in albums_list:
-        """
-        Iterates through albums and inserts them into the database
-        """
-        albums_list = forge_album_tracklist(artist_albums, token)
-        artists['albums'].append(albums_list)
-        print(albums)
-        sql_insert = "INSERT INTO Album (Title, ReleaseDate, CoverImage,ArtistID) VALUES (%s, %s, %s, %s)"
-        cursor.execute(sql_insert, ((remove_special_characters(albums['album_name'])),(albums['release_date']),albums['images'],artists['ArtistsId']))
-        tracks = albums['tracks']
-        for songs in tracks:
-            """"
-            Iterates through songs and inserts them into the database
-            """
-            album_name = albums['album_name']
-            # Your SQL SELECT statement
-            sql_select = f"SELECT AlbumID FROM Album WHERE Title = '{remove_special_characters(album_name)}'"
-            # Execute the SQL statement
-            cursor.execute(sql_select)
+    sql_select = f"SELECT ArtistID FROM Album Artist ArtistID = '{artists['ArtistId']}'"
+     # Execute the SQL statement
+    cursor.execute(sql_select)
             # Fetch the result
-            result = cursor.fetchone()
-            if result:
-                album_id = result[0]
-                print(f"Album ID for '{album_name}': {album_id}")
-                print(songs['track_title'])
-                sql_insert = "INSERT INTO Song (Title,AlbumID,ArtistID) VALUES (%s, %s, %s)"
-                cursor.execute(sql_insert, (songs['track_title'],album_id,artists['ArtistsId']))
-            else:
-                print(f"No album found with the title '{album_name}'")
-    
+    result = cursor.fetchone()
+    print(result)
+    if result:
+        pass
+        """ 
+        Iterates through artists and inserts them into the database
+        """
+        artist_albums = get_artist_albums(token,artists['ArtistId'])
+
+        albums_list = forge_album_tracklist(artist_albums, token)
+        sql_insert = "INSERT INTO Artist (ArtistID, ArtistName, Genre) VALUES (%s, %s, %s)"
+        s = ''.join(str(x) for x in artists['genres'])
+        cursor.execute(sql_insert, (artists['ArtistsId'], artists['ArtistName'], s))
+        for albums in albums_list:
+            """
+            Iterates through albums and inserts them into the database
+            """
+            albums_list = forge_album_tracklist(artist_albums, token)
+            artists['albums'].append(albums_list)
+            print(albums)
+            sql_insert = "INSERT INTO Album (Title, ReleaseDate, CoverImage,ArtistID) VALUES (%s, %s, %s, %s)"
+            cursor.execute(sql_insert, ((remove_special_characters(albums['album_name'])),(albums['release_date']),albums['images'],artists['ArtistsId']))
+            tracks = albums['tracks']
+            for songs in tracks:
+                """"
+                Iterates through songs and inserts them into the database
+                """
+                album_name = albums['album_name']
+                # Your SQL SELECT statement
+                sql_select = f"SELECT AlbumID FROM Album WHERE Title = '{remove_special_characters(album_name)}'"
+                # Execute the SQL statement
+                cursor.execute(sql_select)
+                # Fetch the result
+                result = cursor.fetchone()
+                if result:
+                    album_id = result[0]
+                    print(f"Album ID for '{album_name}': {album_id}")
+                    print(songs['track_title'])
+                    sql_insert = "INSERT INTO Song (Title,AlbumID,ArtistID) VALUES (%s, %s, %s)"
+                    cursor.execute(sql_insert, (songs['track_title'],album_id,artists['ArtistsId']))
+                else:
+                    print(f"No album found with the title '{album_name}'")
+    else:
+        pass
+        
     connection.commit()
     time.sleep(15)
    
